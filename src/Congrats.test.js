aea4a189f1d2e774b13e9f5010328e7f4cb7bd9a
Clannad Key <dgvu2903@gmail.com>
2019-03-17 19:14:28 -0600

    tests for Congrats component and proptypes

---
+++ src/Congrats.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+import Congrats from './Congrats';
+import {findByTestAttr, checkProps} from '../test/testUtils';
+
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+});
+
+const defaultProps = {success: false};
+
+const setUp = (props={}) => {
+  const setUpProps= {...defaultProps, ...props};
+  return shallow(<Congrats {...setUpProps} />);
+}
+
+test("renders without error", () => {
+  const wrapper = setUp();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
+});
+
+test("renders no text when 'success' prop is false", () => {
+  const wrapper = setUp({success: false});
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.text()).toBe("")
+});
+
+test("renders non-empty contrats message when 'success' prop is true", () => {
+  const wrapper = setUp({success: true});
+  const message = findByTestAttr(wrapper, 'congrats-message');
+  expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false};
+  checkProps(Congrats, expectedProps);
+});
